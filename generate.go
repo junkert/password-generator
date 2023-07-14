d1630462990908aa37db0c90bfebdd7955bc9af7
Levi Junkert <ljunkert@genedx.com>
2023-07-14 11:12:43 -0600

    fix: fix printf issue

--- generate.go
+++ generate.go
@@ -1,16 +1,17 @@
 package main
 
 import (
-  "fmt"
-  "github.com/sethvargo/go-password/password"
+	"fmt"
+
+	"github.com/sethvargo/go-password/password"
 )
 
 func main() {
-  // Generate a password that is 64 characters long with 10 digits, 10 symbols,
-  // allowing upper and lower case letters, disallowing repeat characters.
-  res, err := password.Generate(32, 10, 10, false, false)
-  if err != nil {
-    fmt.Printf(err)
-  }
-  fmt.Printf("Password: ", res)
+	// Generate a password that is 64 characters long with 10 digits, 10 symbols,
+	// allowing upper and lower case letters, disallowing repeat characters.
+	res, err := password.Generate(32, 10, 10, false, false)
+	if err != nil {
+		fmt.Printf("%+v", err)
+	}
+	fmt.Printf("Password: ", res)
 }
